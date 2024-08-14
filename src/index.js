ce2240a3fb87e3455b7f12c535d6ecf188f6d8d6
sosoayaen <sosoayaen@gmail.com>
2024-08-14 18:23:10 +0800

    Update index.js
    change to my own domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.thekongfu.com": dockerHub,
+  "quay.thekongfu.com": "https://quay.io",
+  "gcr.thekongfu.com": "https://gcr.io",
+  "k8s-gcr.thekongfu.com": "https://k8s.gcr.io",
+  "k8s.thekongfu.com": "https://registry.k8s.io",
+  "ghcr.thekongfu.com": "https://ghcr.io",
+  "cloudsmith.thekongfu.com": "https://docker.cloudsmith.io",
+  "ecr.thekongfu.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.thekongfu.com": dockerHub,
 };
 
 function routeByHosts(host) {
